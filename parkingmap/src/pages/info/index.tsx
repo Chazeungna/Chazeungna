e46ad02adf27a80c31041c219a7ba5385374dbe0
yunmi099 <yunmi099@ajou.ac.kr>
2023-06-08 02:10:10 +0900

    style: 필터 container 스타일 수정"

--- parkingmap/src/pages/info/index.tsx
+++ parkingmap/src/pages/info/index.tsx
@@ -51,7 +51,7 @@ function ParkingInfo() {
         
     <div className={styles.container}>
             <div style={{display:'flex',}}>
-              <img src='/assets/icons/home.png' width="30px" height="30px" style={{position:'absolute', top:28, left:40}} onClick={()=>navigate('/')}/>
+              <img src='/assets/icons/home.png' width="30px" height="30px" style={{position:'absolute', top:28, left:'3vw'}} onClick={()=>navigate('/')}/>
               <div style={{marginTop: 30, marginBottom:20,height:"20%", fontSize:17}}>
               <span style={{fontSize: 25,fontWeight:400, color: '#775EEE'}}>{selectedSpot.spot_name}</span>
               의 주차정보 입니다</div>
